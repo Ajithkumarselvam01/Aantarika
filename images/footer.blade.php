d0d2c98869bdb364fe474c7047d02c583a70f3c5
AJITHKUMAR S <ajithkumar.s@grocat.in>
2025-01-25 11:29:03 +0530

    Add files via upload

---
+++ images/footer.blade.php
@@ -0,0 +1,142 @@
+<footer class="main-footer dark">
+    <div class="container">
+        <div class="row">
+            <!-- <div class="col-lg-3">
+                <div class="item abot">
+                    <div class="logo-footer mb-20">
+                        <a href="#0" class="logo">
+                            <img src="/assets/img/logo/logo3.png" alt="">
+                        </a>
+                    </div>
+                    <p>
+                        Promising an unparalleled narrative of elegance, excellence, and enduring beauty.
+                    </p>
+                    <div class="social-icon">
+                        <a href="https://www.instagram.com/aantarika_design?igsh=MTAyNHZucW8zMmRw"><i class="fab fa-instagram"></i></a>
+                        <a href="https://youtube.com/@aantarika3184?si=4Rb-AakfkbwEPBcA"><i class="fab fa-youtube"></i></a>
+                        <a href="#0"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#0"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="item fotcont">
+                    <div class="fothead">
+                        <h6>Get in touch</h6>
+                    </div>
+                    <p>+91-9686360414</p> <br>
+                    <p>info@aantarika.com</p>
+                    <p>service@aantarika.com</p>
+                    <p>careers@aantarika.com</p> <br>
+                    <p>546, 9th Cross Rd,
+                        3rd Phase, J. P. Nagar,
+                        Bengaluru, Karnataka 560078</p>
+                </div>
+            </div> -->
+            <div class="col-md-1"></div>
+            <div class="col-md-6">
+                <h2>Get in touch</h2>
+                <h2>with us</h2>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 50px;">
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <p>General Enquiries:</p>
+                info@aantarika.com <br>
+                +91 96863 60414 <br>
+                +91 96060 54606
+            </div>
+            <div class="col-md-3">
+                <p>Services:</p>
+                service@aantarika.com
+            </div>
+            <div class="col-md-3">
+                <p>Careers:</p>
+                careers@aantarika.com
+            </div>
+        </div>
+
+        <!-- <section class="skills-circle section-padding bg-img" data-background="/assets/img/extra/map3.jpg" data-overlay-dark="7" style="height:500px">
+            <a href="">
+                <img src="/assets/img/extra/Location.png" alt="" style="width: 30px; margin-left: 650px; margin-top: -90px">
+            </a>
+        </section> -->
+
+
+        <div class="row" style="margin-top: 40px;">
+            <div class="col-md-1">
+
+            </div>
+            <div class="col-md-5">
+                <p>Promising an unparalleled narrative of elegance, excellence, and enduring beauty.</p>
+            </div>
+            <div class="col-md-2">
+
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-5">
+                <!-- <button type="submit" onclick="openModal()" class="custom-button"><span>Send request</span></button> -->
+                <a onclick="openModal()" class="btn-curve btn-bord btn-lit mt-30">
+                    <span>Send request</span>
+                </a>
+            </div>
+            <div class="col-md-2">
+
+            </div>
+            <div class="col-md-4">
+                <div class="item abot">
+                    <div class="social-icon">
+                        <a href="https://www.instagram.com/aantarika_design?igsh=MTAyNHZucW8zMmRw" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://youtube.com/@aantarika3184?si=4Rb-AakfkbwEPBcA" target="_blank"><i class="fab fa-youtube"></i></a>
+                        <a href="https://wa.me/message/X2GF545W6EEOF1" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                        <a href="https://www.facebook.com/profile.php?id=61550529658790" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#0" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="mt-4">
+            <img id="mapImage" src="/assets/img/extra/map3.jpg" alt="Map Image" usemap="#imageMap">
+
+            <map name="imageMap">
+                <!-- Define the clickable area -->
+                <area id="clickableArea" shape="rect" coords="" href="https://maps.app.goo.gl/cy7txcZcfYwvo3dE6" alt="Clickable Area" target="_blank">
+                <!-- Adjust the 'coords' values to define the coordinates of your clickable area -->
+                <area id="clickableArea1" shape="rect" coords="" href="https://maps.app.goo.gl/iyAznbUHE7EM8AWQ7" alt="Clickable Area" target="_blank">
+            </map>
+        </div>
+    </div>
+    <!-- <div data-background="/assets/img/extra/map3.jpg">
+        <a href="">
+            <img src="/assets/img/extra/map3.jpg" alt="">
+        </a>
+    </div> -->
+
+
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                    <p>
+                            <a href="/privacy_policy">Our Privacy Policy</a>
+                        </p>
+                        <p>COPYRIGHT 2024. ALL RIGHTS RESERVED</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fix-poopup">
+        <a onclick="openModal()" class="btn-curve btn-bord btn-lit">
+                    <span>Send request</span></a>
+    </div>
+</footer>
